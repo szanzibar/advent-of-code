52812d80ee51e79574515fc28a783a80fa142cfb
Steven van Dijk <steven.vandijk@pdq.com>
2021-12-01 14:47:18 +0100

    d1p2 complete

--- lib/1_sonar_sweep/1_sonar_sweep.ex
+++ lib/1_sonar_sweep/1_sonar_sweep.ex
@@ -1,21 +1,36 @@
 defmodule AoC.SonarSweep do
-  @spec sonar_sweep(binary()) :: integer()
-  def sonar_sweep(file) when is_binary(file) do
-    measurements =
-      File.read!(Path.join(__DIR__, file))
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.to_integer(&1))
-
-    sonar_sweep(measurements)
+  defp read_measurements_file(file) do
+    File.read!(Path.join(__DIR__, file))
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer(&1))
   end
 
-  @spec sonar_sweep(list, integer(), integer()) :: integer()
-  def sonar_sweep(measurements, previous \\ nil, total_increase \\ 0)
+  @spec increases(binary()) :: integer()
+  def increases(file) when is_binary(file), do: increases(read_measurements_file(file))
 
-  def sonar_sweep([head | tail], previous, total_increase) do
+  @spec increases(list, integer(), integer()) :: integer()
+  def increases(measurements, previous \\ nil, total_increase \\ 0)
+
+  def increases([head | tail], previous, total_increase) do
     increase = if head > previous, do: 1, else: 0
-    sonar_sweep(tail, head, total_increase + increase)
+    increases(tail, head, total_increase + increase)
   end
 
-  def sonar_sweep([], _previous, total_increase), do: total_increase
+  def increases([], _previous, total_increase), do: total_increase
+
+  @spec window_increases(binary()) :: integer()
+  def window_increases(file) when is_binary(file),
+    do: window_increases(read_measurements_file(file))
+
+  @spec window_increases(list, integer(), integer()) :: integer()
+  def window_increases(measurements, previous \\ nil, total_increase \\ 0)
+
+  def window_increases([_a, _b], _previous, total_increase), do: total_increase
+
+  def window_increases(measurements, previous, total_increase) do
+    [_head | tail] = measurements
+    sum = Enum.take(measurements, 3) |> Enum.sum()
+    increase = if sum > previous, do: 1, else: 0
+    window_increases(tail, sum, total_increase + increase)
+  end
 end
--- test/1_sonar_sweep_test.exs
+++ test/1_sonar_sweep_test.exs
@@ -1,13 +1,21 @@
 defmodule AoC.SonarSweepTest do
   use ExUnit.Case
+  @measurements [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+  @measurements_file "input"
 
   test "example: counts depth measurement increases" do
-    measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-    assert AoC.SonarSweep.sonar_sweep(measurements) == 7
+    assert AoC.SonarSweep.increases(@measurements) == 7
   end
 
   test "real data from file: counts depth measurement increases" do
-    measurements = "input"
-    assert is_integer(AoC.SonarSweep.sonar_sweep(measurements))
+    assert is_integer(AoC.SonarSweep.increases(@measurements_file))
+  end
+
+  test "example: sonar sweep sums" do
+    assert AoC.SonarSweep.window_increases(@measurements) == 5
+  end
+
+  test "real data from file: counts depth measurement window increases" do
+    assert is_integer(AoC.SonarSweep.window_increases(@measurements_file))
   end
 end
