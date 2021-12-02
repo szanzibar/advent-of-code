41038a1bbd3f0e2cc1ce636a737d504a7d10d640
Steven van Dijk <steven.vandijk@pdq.com>
2021-12-02 13:48:22 +0100

    d2p2 complete

--- lib/1_sonar_sweep/day_02/day_02.ex
+++ lib/1_sonar_sweep/day_02/day_02.ex
@@ -7,15 +7,39 @@ defmodule AoC.Day02 do
       |> Enum.chunk_every(2)
       |> Map.new(fn [cmd, ammount] -> {String.to_atom(cmd), String.to_integer(ammount)} end)
     end)
-    |> Enum.reduce(%{}, fn instruction, acc ->
-      Map.merge(instruction, acc, fn _key, v1, v2 ->
-        v1 + v2
-      end)
-    end)
   end
 
   def part1(file) do
-    movements = get_movements(file)
+    movements =
+      get_movements(file)
+      |> Enum.reduce(%{}, fn instruction, acc ->
+        Map.merge(instruction, acc, fn _key, v1, v2 ->
+          v1 + v2
+        end)
+      end)
+
     (movements.down - movements.up) * movements.forward
+  end
+
+  def part2(file) do
+    movements = get_movements(file) |> IO.inspect()
+    part2(movements, 0, 0, 0)
+  end
+
+  def part2([], _aim, position, depth) do
+    depth * position
+  end
+
+  def part2([movement | tail], aim, position, depth) do
+    case movement do
+      %{down: d} ->
+        part2(tail, aim + d, position, depth)
+
+      %{up: u} ->
+        part2(tail, aim - u, position, depth)
+
+      %{forward: f} ->
+        part2(tail, aim, position + f, depth + aim * f)
+    end
   end
 end
