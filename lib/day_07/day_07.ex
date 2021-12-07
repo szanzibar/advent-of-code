de4377448e7ca42c17300f5940dd7105f86569b7
Steven van Dijk <steven.vandijk@pdq.com>
2021-12-07 13:30:05 +0100

    d7p2 complete

--- lib/day_07/day_07.ex
+++ lib/day_07/day_07.ex
@@ -6,50 +6,56 @@ defmodule AoC.Day07 do
   end
 
   def part1(input) do
-    list = get_input(input)
-    find_position(list)
+    calculate_fuel = fn list, align ->
+      Enum.map(list, fn x ->
+        abs(x - align)
+      end)
+      |> Enum.sum()
+    end
+
+    get_input(input)
+    |> find_position(calculate_fuel)
   end
 
-  defp find_position(list) do
+  defp find_position(list, calculate) do
     sorted = Enum.sort(list)
-    zero_cost = calculate_fuel(sorted, 0)
-    find_position(sorted, zero_cost, 0, Enum.count(sorted))
+
+    find_position(
+      sorted,
+      Enum.at(sorted, 0),
+      Enum.at(sorted, Enum.count(sorted) - 1),
+      calculate
+    )
   end
 
-  defp find_position(list, current_cost, min, max) do
-    min |> IO.inspect(label: "min")
-    max |> IO.inspect(label: "max")
-    mid = ((max + min) / 2) |> round() |> IO.inspect(label: "mid")
-    mid_cost = calculate_fuel(list, Enum.at(list, mid)) |> IO.inspect(label: "mid cost")
-    current_cost |> IO.inspect(label: "current cost")
+  defp find_position(list, min, max, calculate) do
+    mid = ((max + min) / 2) |> round()
+    mid_cost = calculate.(list, mid)
+    cost_above = calculate.(list, mid + 1)
+    cost_below = calculate.(list, mid - 1)
 
     cond do
-      mid < 0 ->
-        mid
+      mid_cost <= cost_above && mid_cost <= cost_below ->
+        mid_cost
 
-      mid_cost == current_cost ->
-        calculate_fuel(list, Enum.at(list, mid))
+      cost_above > mid_cost ->
+        find_position(list, min, mid, calculate)
 
-      mid_cost > current_cost ->
-        find_position(list, current_cost, min, mid - 1)
-
-      mid_cost < current_cost ->
-        find_position(list, mid_cost, mid, max)
+      cost_below > mid_cost ->
+        find_position(list, mid, max, calculate)
     end
   end
 
-  def fuel(input, number) do
-    get_input(input) |> calculate_fuel(number)
-  end
-
-  defp calculate_fuel(list, align) do
-    Enum.map(list, fn x ->
-      abs(x - align)
-    end)
-    |> Enum.sum()
-  end
-
   def part2(input) do
-    input |> IO.inspect()
+    calculate_fuel = fn list, align ->
+      Enum.map(list, fn x ->
+        distance = abs(x - align)
+        for(y <- 1..distance, do: y) |> Enum.sum()
+      end)
+      |> Enum.sum()
+    end
+
+    get_input(input)
+    |> find_position(calculate_fuel)
   end
 end
--- test/day_07_test.exs
+++ test/day_07_test.exs
@@ -14,4 +14,16 @@ defmodule AoC.Day07.Test do
     Logger.info("part 1 results: #{results}")
     assert results == 344_605
   end
+
+  @tag :d7p2
+  test "example: part2" do
+    assert part2("test_input") == 168
+  end
+
+  @tag :d7p2
+  test "real data: part2" do
+    results = part2("input")
+    Logger.info("part 2 results: #{results}")
+    assert results == 93_699_985
+  end
 end
