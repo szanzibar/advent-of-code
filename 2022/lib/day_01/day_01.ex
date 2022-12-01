72f48f4bb016d0a093490675c90ca11b160e3379
Steven van Dijk <email@stevenvandijk.com>
2022-12-01 09:38:44 -0700

    set up day01

---
+++ 2022/lib/day_01/day_01.ex
@@ -0,0 +1,25 @@
+defmodule AoC2022.Day01 do
+  alias AoC2022.Utils
+
+  defp get_input(file) do
+    File.read!(Path.join(__DIR__, file))
+  end
+
+  @doc """
+      iex> part1("test_input")
+      nil
+
+  """
+  def part1(input_file) do
+    get_input(input_file)
+  end
+
+  @doc """
+      iex> part2("test_input")
+      nil
+
+  """
+  def part2(input_file) do
+    get_input(input_file)
+  end
+end
