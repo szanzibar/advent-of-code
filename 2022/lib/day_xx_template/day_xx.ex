38dd962409e97d07e692450d7ad8235ba2a502c4
Steven van Dijk <email@stevenvandijk.com>
2022-12-01 10:07:27 -0700

    improve template tests

--- 2022/lib/day_xx_template/day_xx.ex
+++ 2022/lib/day_xx_template/day_xx.ex
@@ -9,16 +9,23 @@ defmodule AoC2022.Dayxx do
       iex> part1("test_input")
       nil
 
+      iex> part1("input")
+      nil
+
   """
   def part1(input_file) do
     get_input(input_file)
   end
 
   @doc """
-      iex> part2("test_input")
-      nil
+      # iex> part2("test_input")
+      # nil
+
+      # iex> part2("input")
+      # nil
 
   """
+
   def part2(input_file) do
     get_input(input_file)
   end
