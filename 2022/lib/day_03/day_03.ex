55d6743c1dd67f49b984bc99d2c7bc7ee6f556d0
Steven van Dijk <email@stevenvandijk.com>
2022-12-02 22:56:02 -0700

    day 3 complete

---
+++ 2022/lib/day_03/day_03.ex
@@ -0,0 +1,62 @@
+defmodule AoC2022.Day03 do
+  alias AoC2022.Utils
+
+  def get_input(file) do
+    File.read!(Path.join(__DIR__, file)) |> String.split("\n") |> Enum.map(&String.to_charlist/1)
+  end
+
+  @doc """
+      iex> part1("test_input")
+      157
+
+      iex> part1("input")
+      8240
+
+  """
+  def part1(input_file) do
+    get_input(input_file)
+    |> Enum.map(fn rucksack ->
+      length = Enum.count(rucksack)
+      {comp1, comp2} = Enum.split(rucksack, div(length, 2))
+
+      set1 = MapSet.new(comp1)
+      set2 = MapSet.new(comp2)
+
+      MapSet.intersection(set1, set2)
+      |> MapSet.to_list()
+      |> get_value()
+    end)
+    |> Enum.sum()
+  end
+
+  defp get_value([character]) do
+    if character < 97 do
+      character - 65 + 27
+    else
+      character - 97 + 1
+    end
+  end
+
+  @doc """
+      iex> part2("test_input")
+      70
+
+      iex> part2("input")
+      2587
+
+  """
+  def part2(input_file) do
+    get_input(input_file)
+    |> Enum.map(&MapSet.new/1)
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn group ->
+      group
+      |> Enum.reduce(fn rucksack, acc ->
+        MapSet.intersection(rucksack, acc)
+      end)
+      |> MapSet.to_list()
+      |> get_value()
+    end)
+    |> Enum.sum()
+  end
+end
---
+++ 2022/lib/day_03/day_03_test.exs
@@ -0,0 +1,4 @@
+defmodule AoC2022.Day03.Test do
+  use ExUnit.Case, async: true
+  doctest AoC2022.Day03, import: true
+end
