be4157187e7655cabfa7eb84c33b57cbd0d9bef6
Steven van Dijk <email@stevenvandijk.com>
2023-12-01 13:12:39 +0100

    day 1 part 2

--- 2023/lib/day01/day_01.ex
+++ 2023/lib/day01/day_01.ex
@@ -9,27 +9,59 @@ defmodule AoC2023.Day01 do
     142
 
     iex> part1("input")
-    nil
+    54916
 
   """
   def part1(input_file) do
     get_input(input_file)
     |> Enum.reduce(0, fn line, acc ->
-      numbers = Regex.scan(~r/\d/, line) |> List.flatten()
-      {num, _} = [Enum.at(numbers, 0), Enum.at(numbers, -1)] |> Enum.join() |> Integer.parse()
-      acc + num
+      Regex.scan(~r/\d/, line)
+      |> List.flatten()
+      |> sum_first_last(acc)
     end)
   end
 
   @doc """
-    # iex> part2("test_input")
-    # nil
+    iex> part2("test_input_p2")
+    281
 
-    # iex> part2("input")
-    # nil
+    iex> part2("input")
+    54728
 
   """
   def part2(input_file) do
     get_input(input_file)
+    |> Enum.reduce(0, fn line, acc ->
+      Regex.scan(
+        ~r/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/,
+        line,
+        capture: :all_but_first
+      )
+      |> List.flatten()
+      |> Enum.map(&clean_numbers/1)
+      |> sum_first_last(acc)
+    end)
+  end
+
+  defp sum_first_last(num_list, acc) do
+    {num, _} =
+      [Enum.at(num_list, 0), Enum.at(num_list, -1)]
+      |> Enum.join()
+      |> Integer.parse()
+
+    num + acc
+  end
+
+  defp clean_numbers(string) do
+    string
+    |> String.replace("one", "1")
+    |> String.replace("two", "2")
+    |> String.replace("three", "3")
+    |> String.replace("four", "4")
+    |> String.replace("five", "5")
+    |> String.replace("six", "6")
+    |> String.replace("seven", "7")
+    |> String.replace("eight", "8")
+    |> String.replace("nine", "9")
   end
 end
