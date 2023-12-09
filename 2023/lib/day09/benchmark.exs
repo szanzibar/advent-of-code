d8367df9cd099c1b2b6a904677c7f39d733cba23
Steven van Dijk <email@stevenvandijk.com>
2023-12-09 09:08:08 +0100

    update template & start day 9

---
+++ 2023/lib/day09/benchmark.exs
@@ -0,0 +1,7 @@
+alias AoC2023.Day09
+alias AoC2023.Day09.Input
+
+Benchee.run(%{
+  "part1" => fn -> Day09.part1(Input.input()) end,
+  "part2" => fn -> Day09.part2(Input.input()) end
+})
---
+++ 2023/lib/day09/day_09_test.exs
@@ -0,0 +1,4 @@
+defmodule AoC2023.Day09.Test do
+  use ExUnit.Case, async: true
+  doctest AoC2023.Day09, import: true
+end
