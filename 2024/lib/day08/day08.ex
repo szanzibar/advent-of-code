4c5af46f3b4f69ca868ad27a01ef4a7d4bb28130
Steven van Dijk <email@stevenvandijk.com>
2024-12-08 14:11:10 +0100

    day08 part2

--- 2024/lib/day08/day08.ex
+++ 2024/lib/day08/day08.ex
@@ -24,8 +24,8 @@ defmodule AoC2024.Day08 do
 
   """
   def part1(input) do
-    map = parse_input(input) |> dbg
-    distinct_frequencies = distinct_frequencies(map) |> dbg
+    map = parse_input(input)
+    distinct_frequencies = distinct_frequencies(map)
 
     pairs_by_freq = get_freq_pairs(distinct_frequencies, map)
 
@@ -44,25 +44,9 @@ defmodule AoC2024.Day08 do
         new_coord1 = {col1 + col_offset, row1 + row_offset}
         new_coord2 = {col2 + col_offset * -1, row2 + row_offset * -1}
 
-        inner_acc_map =
-          Map.get(inner_acc_map, new_coord1)
-          |> case do
-            nil -> inner_acc_map
-            {freq, _} -> Map.put(inner_acc_map, new_coord1, {freq, "#"})
-          end
-
-        inner_acc_map =
-          inner_acc_map
-          |> Map.get(new_coord2)
-          |> case do
-            nil -> inner_acc_map
-            {freq, _} -> Map.put(inner_acc_map, new_coord2, {freq, "#"})
-          end
-
-        inner_acc_map
+        add_antinode_list(inner_acc_map, [new_coord1, new_coord2])
       end)
     end)
-    |> print_map()
   end
 
   defp get_freq_pairs(frequencies, map) do
@@ -70,7 +54,6 @@ defmodule AoC2024.Day08 do
       coords =
         get_freq_coords(freq, map)
         |> pair_recursion()
-        |> dbg
 
       {freq, coords}
     end)
@@ -113,15 +96,71 @@ defmodule AoC2024.Day08 do
   defp print_map(what), do: what
 
   @doc """
-    # iex> part2(AoC2024.Day08.Input.test_input())
-    # nil
+    iex> part2(AoC2024.Day08.Input.test_input())
+    34
 
-    # iex> part2(AoC2024.Day08.Input.input())
-    # nil
+    iex> part2(AoC2024.Day08.Input.input())
+    1030
 
   """
 
   def part2(input) do
-    parse_input(input)
+    map = parse_input(input)
+    distinct_frequencies = distinct_frequencies(map)
+
+    pairs_by_freq = get_freq_pairs(distinct_frequencies, map)
+
+    map = build_antinode_map2(pairs_by_freq, map)
+
+    Enum.count(map, fn {_coord, {_freq, antinode}} -> !is_nil(antinode) end)
+  end
+
+  defp build_antinode_map2(pairs_by_freq, map) do
+    {{max_col, _}, _} = Enum.max_by(map, fn {{col, _}, _} -> col end)
+    {{_, max_row}, _} = Enum.max_by(map, fn {{_, row}, _} -> row end)
+
+    pairs_by_freq
+    |> Enum.reduce(map, fn {_freq, pairs}, acc_map ->
+      pairs
+      |> Enum.reduce(acc_map, fn {{col1, row1}, {col2, row2}}, inner_acc_map ->
+        col_offset = col1 - col2
+        row_offset = row1 - row2
+
+        new_coords_pos =
+          next_coord({col2, row2}, {col_offset, row_offset}, {max_col, max_row}, [])
+
+        new_coords_neg =
+          next_coord({col1, row1}, {col_offset * -1, row_offset * -1}, {max_col, max_row}, [])
+
+        add_antinode_list(inner_acc_map, new_coords_pos ++ new_coords_neg)
+      end)
+    end)
+    |> print_map()
+  end
+
+  defp next_coord({col, row}, {c_offset, r_offset}, {c_max, r_max}, acc)
+       when col + c_offset > c_max or row + r_offset > r_max or col + c_offset < 0 or
+              row + r_offset < 0,
+       do: acc
+
+  defp next_coord({col, row}, {c_offset, r_offset}, maxes, acc) do
+    new_coord = {col + c_offset, row + r_offset}
+    new_acc = [new_coord | acc]
+
+    next_coord(new_coord, {c_offset, r_offset}, maxes, new_acc)
+  end
+
+  defp add_antinode_list(map, coords) do
+    Enum.reduce(coords, map, fn coord, acc_map ->
+      add_antinode(acc_map, coord)
+    end)
+  end
+
+  defp add_antinode(map, coord) do
+    Map.get(map, coord)
+    |> case do
+      nil -> map
+      {freq, _} -> Map.put(map, coord, {freq, "#"})
+    end
   end
 end
