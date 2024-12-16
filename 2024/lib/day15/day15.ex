aff6b042a212276c3087744f8a66ac931553f036
Steven van Dijk <email@stevenvandijk.com>
2024-12-16 11:16:57 +0200

    day15 part2

--- 2024/lib/day15/day15.ex
+++ 2024/lib/day15/day15.ex
@@ -28,8 +28,8 @@ defmodule AoC2024.Day15 do
     iex> part1(AoC2024.Day15.Input.test_input2())
     10092
 
-    # iex> part1(AoC2024.Day15.Input.input())
-    # 1526673
+    iex> part1(AoC2024.Day15.Input.input())
+    1526673
 
   """
   def part1(input) do
@@ -51,33 +51,70 @@ defmodule AoC2024.Day15 do
   defp move_robot(grid, [move | moves]) do
     {robot_coord, robot} = Enum.find(grid, fn {_, robot} -> robot == "@" end)
 
-    {_, grid} = push_object(grid, robot_coord, move, robot)
+    grid = push_object_moves(grid, robot_coord, move, robot) |> push_object(grid)
+
     move_robot(grid, moves)
   end
 
-  defp push_object(grid, coord, direction, object) do
+  defp push_object(false, grid), do: grid
+
+  defp push_object({true, moves}, grid) do
+    {deletes, moves} = Enum.split_with(moves, fn {_coord, char} -> char == "." end)
+
+    grid =
+      Enum.reduce(deletes, grid, fn {coord, object}, grid_acc ->
+        Map.put(grid_acc, coord, object)
+      end)
+
+    Enum.reduce(moves, grid, fn {coord, object}, grid_acc ->
+      Map.put(grid_acc, coord, object)
+    end)
+  end
+
+  defp push_object_moves(grid, coord, direction, object) do
     next_coord = next_coord(coord, direction)
     next_object = Map.get(grid, next_coord)
 
     case next_object do
       "#" ->
-        {false, grid}
+        false
 
       "." ->
         # move to this location, backfill previous location to "."
-        {true, Map.put(grid, coord, ".") |> Map.put(next_coord, object)}
+        {true, [{coord, "."}, {next_coord, object}]}
 
-      "O" ->
-        push_object(grid, next_coord, direction, "O")
+      next_object when next_object in ["[", "]"] and direction in ["^", "v"] ->
+        other_next_object = if next_object == "[", do: "]", else: "["
+        other_next_coord = get_other_part_coord(next_coord, next_object)
+
+        next_result = push_object_moves(grid, next_coord, direction, next_object)
+        other_result = push_object_moves(grid, other_next_coord, direction, other_next_object)
+
+        case {next_result, other_result} do
+          {{true, next}, {true, other}} ->
+            my_moves = [{coord, "."}, {next_coord, object}, {other_next_coord, "."}]
+            all_moves = my_moves ++ next ++ other
+
+            {true, all_moves}
+
+          _ ->
+            false
+        end
+
+      next_object when next_object in ["O", "[", "]"] ->
+        push_object_moves(grid, next_coord, direction, next_object)
         |> case do
-          {false, grid} ->
-            {false, grid}
+          false ->
+            false
 
-          {true, pushed_grid} ->
-            {true, Map.put(pushed_grid, coord, ".") |> Map.put(next_coord, object)}
+          {true, list_of_moves} ->
+            {true, [{coord, "."}, {next_coord, object} | list_of_moves]}
         end
     end
   end
+
+  defp get_other_part_coord(coord, "["), do: next_coord(coord, ">")
+  defp get_other_part_coord(coord, "]"), do: next_coord(coord, "<")
 
   defp next_coord({row, col}, direction) do
     {row_offset, col_offset} = coord_offset(direction)
@@ -89,8 +126,9 @@ defmodule AoC2024.Day15 do
   defp coord_offset(">"), do: {0, 1}
   defp coord_offset("v"), do: {1, 0}
 
-  defp print_map(map) do
+  defp print_map(map, label \\ "") do
     IO.inspect("")
+    IO.inspect(label)
 
     Enum.sort_by(map, fn {coord, _} -> coord end)
     |> Enum.chunk_by(fn {{row, _col}, _} -> row end)
@@ -133,63 +171,25 @@ defmodule AoC2024.Day15 do
   end
 
   @doc """
-    iex> part2(AoC2024.Day15.Input.test_input3())
-    nil
+    iex> part2(AoC2024.Day15.Input.test_input2())
+    9021
 
-    # iex> part2(AoC2024.Day15.Input.test_input2())
-    # nil
-
-    # iex> part2(AoC2024.Day15.Input.input())
-    # nil
+    iex> part2(AoC2024.Day15.Input.input())
+    1535509
 
   """
   def part2(input) do
     {grid, moves} = parse_input2(input)
     print_map(grid)
-    moved_grid = move_robot2(grid, moves)
-    sum_gps_coords(moved_grid)
+    moved_grid = move_robot(grid, moves) |> print_map()
+    sum_gps_coords2(moved_grid)
   end
 
-  defp move_robot2(grid, []), do: grid
-
-  defp move_robot2(grid, [move | moves]) do
-    {robot_coord, robot} = Enum.find(grid, fn {_, robot} -> robot == "@" end)
-
-    {_, grid} = push_object2(grid, robot_coord, move, robot)
-    move_robot2(grid, moves)
+  defp sum_gps_coords2(moved_grid) do
+    Enum.filter(moved_grid, fn {_coord, object} -> object == "[" end)
+    |> Enum.map(fn {{row, col}, _} ->
+      100 * row + col
+    end)
+    |> Enum.sum()
   end
-
-  defp push_object2(grid, coord, direction, object) do
-    next_coord2 = next_coord2(coord, direction)
-    next_object = Map.get(grid, next_coord2)
-
-    case next_object do
-      "#" ->
-        {false, grid}
-
-      "." ->
-        # move to this location, backfill previous location to "."
-        {true, Map.put(grid, coord, ".") |> Map.put(next_coord2, object)}
-
-      "O" ->
-        push_object2(grid, next_coord2, direction, "O")
-        |> case do
-          {false, grid} ->
-            {false, grid}
-
-          {true, pushed_grid} ->
-            {true, Map.put(pushed_grid, coord, ".") |> Map.put(next_coord2, object)}
-        end
-    end
-  end
-
-  defp next_coord2({row, col}, direction) do
-    {row_offset, col_offset} = coord_offset2(direction)
-    {row + row_offset, col + col_offset}
-  end
-
-  defp coord_offset2("<"), do: {0, -1}
-  defp coord_offset2("^"), do: {-1, 0}
-  defp coord_offset2(">"), do: {0, 1}
-  defp coord_offset2("v"), do: {1, 0}
 end
